4422a362f19e3a2b702209c31aea3af3ed624faa
Jinho <dn70032615@gmail.com>
2025-06-02 14:48:02 +0900

    경로 수정

--- app/models/models.py
+++ app/models/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text
 from sqlalchemy.orm import relationship
 from datetime import datetime
-from app.database import Base
+from database import Base
 
 class User(Base):
     __tablename__ = "user"
