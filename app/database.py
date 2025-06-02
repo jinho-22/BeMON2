fd2877382601a37edc0576164845948784b7595b
Jinho <dn70032615@gmail.com>
2025-06-02 11:14:37 +0900

    database.py 수정

--- app/database.py
+++ app/database.py
@@ -12,12 +12,12 @@ MYSQL_USER = os.environ["MYSQL_USER"]
 MYSQL_PASSWORD = os.environ["MYSQL_PASSWORD"]
 MYSQL_HOST = os.environ["MYSQL_HOST"]
 MYSQL_PORT = os.environ["MYSQL_PORT"]
-MYSQL_DB = os.environ["MYSQL_DB"]
+MYSQL_DATABASE = os.environ["MYSQL_DATABASE"]
 
 
 # SQLAlchemy 접속 URL 구성
 DATABASE_URL = (
-    f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
+    f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
 )
 
 # 엔진 및 세션 생성
